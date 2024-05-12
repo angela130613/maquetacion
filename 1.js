77da1879a317dac962d3c08b237848bcc0a208dc
Angela Maria Yepes Ramirez <angelyep76@gmail.com>
2024-05-11 22:52:19 -0500

    Proceso commit

---
+++ 1.js
@@ -0,0 +1,34 @@
+<div class="card">
+            <h2>CHILE</h2>
+            <img src="Profile/Chile.jpg" alt="CHILE">
+        </div>
+
+        <div class="card">
+            <h2>CHINA</h2>
+            <img src="Profile/China.jpg" alt="CHINA">
+        </div>
+
+        <div class="card">
+            <h2>JAPON</h2>
+            <img src="Profile/Japon.jpg" alt="JAPON">
+        </div>
+
+        <div class="card">
+            <h2>LONDRES</h2>
+            <img src="Profile/Londres.jpg" alt="LONDRES">
+        </div>
+
+        <div class="card">
+            <h2>NUEVA ORLEANS</h2>
+            <img src="Profile/Nueva Orleans.jpg" alt="NUEVA ORLEANS">
+        </div>
+
+        <div class="card">
+            <h2>NUEVA YORK</h2>
+            <img src="Profile/Nueva York.jpg" alt="NUEVA YORK">
+        </div>
+
+        <div class="card">
+            <h2>PANAMA</h2>
+            <img src="Profile/Panama.jpg" alt="PANAMA">
+        </div>
